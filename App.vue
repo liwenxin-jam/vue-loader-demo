f5fbb9cf52a2f72c0c260a6eabed452311a0910a
liwenxin-jam <mxlwx@126.com>
2017-09-06 16:41:23 +0800

    Add files via upload
    代码提交，无依赖包

---
+++ App.vue
@@ -0,0 +1,28 @@
+<template>
+	<h1>Welcome Vue</h1>
+	<h2 @click="change">{{msg}}</h2>
+	<my-menu></my-menu>
+</template>
+<script>
+	import Menu from './components/Menu.vue'
+	export default{
+		data(){
+			return {
+				msg: 'Welcome Vue Jam'
+			}
+		},
+		methods:{
+			change(){ 
+				this.msg = 'wahaha'
+			}
+		},
+		components:{
+			'my-menu' : Menu
+		}
+	}
+</script>
+<style>
+	body{
+		background: #ccc;
+	}
+</style>
